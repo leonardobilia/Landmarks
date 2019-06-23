9904eec075ead5dacc277a225be5f894f0359026
Leonardo Bilia <leonardo.bilia@gmail.com>
2019-06-23 01:09:41 -0300

    Create a Custom Image View

---
+++ Landmarks/Views/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Leonardo Bilia on 6/23/19.
+//  Copyright © 2019 Leonardo Bilia. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Image("turtlerock")
+            .resizable()
+            .frame(width: 220, height: 220)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
--- README.md
+++ README.md
@@ -12,6 +12,8 @@ You can find the complete tutorial, following each stage of the project through 
 **Section 1:** Create a New Project and Explore the Canvas *(0.0.1)*<br>
 **Section 2:** Customize the Text View *(0.0.2)*<br>
 **Section 3:** Combine Views Using Stacks *(0.0.3)*<br>
+**Section 4:** Create a Custom Image View *(0.0.4)*<br>
 ![](Documentation/images/T01-S01-IMG01.png)
 ![](Documentation/images/T01-S02-IMG01.png)
 ![](Documentation/images/T01-S03-IMG01.png)
+![](Documentation/images/T01-S04-IMG01.png)
